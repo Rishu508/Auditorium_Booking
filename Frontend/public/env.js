6d26c038fc07b254c32a7315ab34268811971446
Rishabh Kumar <rishabhkumar97348@gmail.com>
2026-01-02 22:59:35 +0530

    Update frontend origin

--- Frontend/public/env.js
+++ Frontend/public/env.js
@@ -1,5 +1,5 @@
 // env.js
 window._env_ = {
-  BACKEND_URL: "https://auditorium-booking-bq66.onrender.com",
-  FRONTEND_URL: "http://localhost:3000",
+  BACKEND_URL: "https://auditorium-booking-rpsd.onrender.com",
+  FRONTEND_URL: "https://audibooking.netlify.app/",
 };
